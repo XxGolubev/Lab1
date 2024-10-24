a1e441a7dfbbd6c9ded1d02c8a7cabce0608cf37
XxGolubev <158230435+XxGolubev@users.noreply.github.com>
2024-10-24 11:15:49 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import java.io.PrintStream;
 import java.util.Scanner;
 public class Main {
+    // Объявляем объект класса Scanner для ввода данных
     public static Scanner in = new Scanner(System.in);
+    // Объявляем объект класса PrintStream для вывода данных
     public static PrintStream out = System.out;
     public static void main(String[] args) {
+        // Считывание двух вещественных чисел a и b из консоли
         double a = in.nextDouble(), b =in.nextDouble();
+        // нахождение решений неравенства
         if (a == 0)
             out.printf("x<" + "%.1f" + " or x>" + "%.1f" ,b ,b);
         else if (a > 0)
